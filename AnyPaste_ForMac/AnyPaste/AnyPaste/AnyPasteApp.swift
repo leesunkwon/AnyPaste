7dfe3cc0e1946bc82917bd7f9a6eab30edb533b3
스윗남 (Kotlinsun) <lsk859797@gmail.com>
2026-08-22 20:57:29 +0900

    맥 프로젝트 세팅

---
+++ AnyPaste_ForMac/AnyPaste/AnyPaste/AnyPasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnyPasteApp.swift
+//  AnyPaste
+//
+//  Created by sunkwon on 8/22/26.
+//
+
+import SwiftUI
+
+@main
+struct AnyPasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
